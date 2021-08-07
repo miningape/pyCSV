cff7d455534b679a2a6ab7048b3906c6478e3cec
Kyle Johnson <miningape@gmail.com>
2021-08-08 01:07:53 +0200

    made it look nicer

--- csvhelper.py
+++ csvhelper.py
@@ -8,15 +8,13 @@ def read(file, converter=int):
   returnArray = []
 
   # Open the file in a variable called csvfile
-  with open(file, newline='') as csvfile:
-    # Read the file we opened, storing each line of the file in an iterator(array)
-    csvreader = csv.reader(csvfile, delimiter=',')
+  with open(file, newline='') as csvfile:                                                   
+    csvreader = csv.reader(csvfile, delimiter=',')  # Read the file we opened, storing each line of the file in an iterator(array)
     
-    for row in csvreader:   # Iterate through each line
-      for item in row:      # Iterate through each item in the row
-        if item:            # If the item exists, i.e. not an empty string: ''
-          # Append the item to our return array, but first we convert it to the type we want
-          returnArray.append( converter(item) )
+    for line in csvreader:                          # Iterate through each line
+      for item in line:                             # Iterate through each item in the row
+        if item:                                    # If the item exists, i.e. not an empty string: ''
+          returnArray.append( converter(item) )     # Append the item to our return array, but first we convert it to the type we want
 
   # Return the array we just built
   return returnArray
